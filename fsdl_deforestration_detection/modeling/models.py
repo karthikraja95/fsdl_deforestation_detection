3727a7b2efeed94dc8b4fd54b9cbfd1db9f28fcc
AndreCNF <andrecnf@gmail.com>
2021-04-19 01:18:27 +0100

    Fix train script to use functional model so as to be compatible with W&B

--- fsdl_deforestration_detection/modeling/models.py
+++ fsdl_deforestration_detection/modeling/models.py
@@ -24,7 +24,11 @@ class ResNet(tf.keras.Model):
         task: str = "orig_labels",
     ):
         """A ResNet50 model that can be pretrained or trained from scratch,
-        adapting to each relevant variation in this project.
+        adapting to each relevant variation in this project. This is the
+        model class version.
+
+        WARNING: Although this seems more clean, it doesn't work well with
+        Weights and Biases' callback. Please use func_resnet instead.
 
         Args:
             pretrain_dataset (str, optional): The dataset in which the model
@@ -79,3 +83,64 @@ class ResNet(tf.keras.Model):
         x = data_augmentation(x)
         x = self.core(x)
         return self.classifier(x)
+
+
+def func_resnet(
+    pretrain_dataset: str = None,
+    pooling: str = "max",
+    task: str = "orig_labels",
+):
+    """A ResNet50 model that can be pretrained or trained from scratch,
+    adapting to each relevant variation in this project. This is the
+    functional API version.
+
+    Works well with Weights and Biases' callback.
+
+    Args:
+        pretrain_dataset (str, optional): The dataset in which the model
+        is pretrained. If left unspecified, the model starts with random
+        weights. Available options are "imagenet" and "bigearthnet".
+        Defaults to None.
+        pooling (str, optional): The type of global pooling to perform
+        after the ResNet layers. Available options are "max" and "avg".
+        Defaults to "max".
+        task (str, optional): The task on which the model will be trained
+        or fine-tuned on. Available options are "orig_labels" (original
+        labels from the Kaggle challenge) and "deforestation".
+        Defaults to "orig_labels".
+
+    Raises:
+        Exception: [description]
+    """
+    inputs = layers.Input(shape=(256, 256, 3))
+    if task == "orig_labels":
+        n_outputs = 17
+    elif task == "deforestation":
+        n_outputs = 1
+    else:
+        raise Exception(
+            f'ERROR: Unrecognized task "{task}". Please select one of "orig_labels" or "deforestation".'
+        )
+    if pretrain_dataset == "bigearthnet":
+        # TensorFlow Hub modules require data in a [0, 1] range
+        # stats estimated from subset of data in `02_eda_amazon_planet` notebook
+        x = Normalization(
+            mean=[79.67114306, 87.08461826, 76.46177919],
+            variance=[1857.54070494, 1382.94249315, 1266.69265399],
+        )(inputs)
+        x = data_augmentation(x)
+        x = hub.KerasLayer(
+            "https://tfhub.dev/google/remote_sensing/bigearthnet-resnet50/1"
+        )(x)
+    else:
+        # Using TensorFlow's ResNet-specific preprocessing
+        x = preprocess_input(x)
+        x = data_augmentation(x)
+        x = ResNet50(
+            include_top=False,
+            weights=pretrain_dataset,
+            pooling=pooling,
+        )(x)
+    outputs = layers.Dense(n_outputs, activation="sigmoid")(x)
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+    return model
--- fsdl_deforestration_detection/modeling/train.py
+++ fsdl_deforestration_detection/modeling/train.py
@@ -2,20 +2,26 @@ import argparse
 import sys
 import pandas as pd
 import numpy as np
+import random
 import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow_addons.metrics import FBetaScore
-from tensorflow.keras import optimizers, callbacks
+from tensorflow.keras import optimizers, callbacks, Model
 from tensorflow.data.experimental import AUTOTUNE
 from tqdm.auto import tqdm
 from tqdm.keras import TqdmCallback
 import wandb
 from wandb.keras import WandbCallback
 
-sys.path.append("../data/")
-sys.path.append("../modeling/")
+sys.path.append(
+    "fsdl_deforestration_detection/fsdl_deforestration_detection/data/"
+)
+sys.path.append(
+    "fsdl_deforestration_detection/fsdl_deforestration_detection/modeling/"
+)
 import data_utils
-from models import ResNet
+import model_utils
+from models import ResNet, func_resnet
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -24,13 +30,14 @@ parser.add_argument(
     help="use a ResNet pretrained on BigEarthNet",
 )
 parser.add_argument(
-    "--lr",
+    "--learning_rate",
     default=0.03,
     help="initial learning rate",
 )
 parser.add_argument(
     "--epochs",
     default=[50, 50],
+    nargs="+",
     help="number of epochs to train on",
 )
 parser.add_argument(
@@ -45,7 +52,8 @@ parser.add_argument(
 )
 parser.add_argument(
     "--task",
-    default=["orig_label", "deforestation"],
+    default=["orig_labels", "deforestation"],
+    nargs="+",
     help="random seed for reproducible results",
 )
 args = parser.parse_args()
@@ -55,6 +63,7 @@ if not isinstance(args.task, list):
     args.task = list(args.task)
 if not isinstance(args.epochs, list):
     args.epochs = list(args.epochs)
+args.epochs = [int(epochs) for epochs in args.epochs]
 if args.pretrained:
     assert (
         len(args.epochs) == len(args.task) * 2,
@@ -74,7 +83,11 @@ else:
         "As such, we expect len(args.epochs) == len(args.task) * 2 - 1. "
         f"Currently specified {len(args.epochs)} epoch sets and {len(args.task)} tasks.",
     )
-opt = optimizers.Adam(learning_rate=args.lr)
+# Set the random seed for reproducibility
+tf.random.set_seed(0)
+random.seed(0)
+np.random.seed(0)
+opt = optimizers.Adam(learning_rate=args.learning_rate)
 loss = "binary_crossentropy"
 count = 0
 for task in tqdm(args.task, desc="Tasks"):
@@ -108,19 +121,22 @@ for task in tqdm(args.task, desc="Tasks"):
     train_set = train_set.skip(int(0.1 * n_samples))
     val_set = train_set.take(int(0.1 * n_samples))
     train_set = (
-        train_set.cache()
+        train_set
+        #  .cache()
         .shuffle(buffer_size=1000)
         .batch(args.batch_size)
         .prefetch(AUTOTUNE)
     )
     val_set = (
-        val_set.cache()
+        val_set
+        # .cache()
         .shuffle(buffer_size=1000)
         .batch(args.batch_size)
         .prefetch(AUTOTUNE)
     )
     test_set = (
-        test_set.cache()
+        test_set
+        # .cache()
         .shuffle(buffer_size=1000)
         .batch(args.batch_size)
         .prefetch(AUTOTUNE)
@@ -130,21 +146,39 @@ for task in tqdm(args.task, desc="Tasks"):
     else:
         pretrain_dataset = None
     # Load the model
-    model = ResNet(pretrain_dataset=pretrain_dataset, pooling="max", task=task)
+    # model = ResNet(pretrain_dataset=pretrain_dataset, pooling="max", task=task)
+    model = func_resnet(
+        pretrain_dataset=pretrain_dataset, pooling="max", task=task
+    )
+    if task == "orig_labels":
+        n_outputs = 17
+    elif task == "deforestation":
+        n_outputs = 1
+    else:
+        raise Exception(
+            f'ERROR: Unrecognized task "{task}". Please select one of "orig_labels" or "deforestation".'
+        )
     model_metrics = [
         "accuracy",
-        FBetaScore(num_classes=model.n_outputs, average="macro", beta=2.0),
+        FBetaScore(num_classes=n_outputs, average="macro", beta=2.0),
     ]
     wandb.init(
         project="fsdl_deforestation_detection",
+        entity="fsdl-andre-karthik",
         tags="mvp",
         reinit=True,
         config={**vars(args), **dict(current_task=task)},
     )
+    # The epoch on which to start the full model training
+    initial_epoch = 0
     if args.pretrained or count > 0:
         # Train initially the final layer
-        model.core.trainable = False
-        model.classifier = layers.Dense(model.n_outputs, activation="sigmoid")
+        model.layers[-2].trainable = False
+        if count > 0:
+            outputs = layers.Dense(n_outputs, activation="sigmoid")(
+                model.layers[-2].output
+            )
+            model = Model(input=model.input, output=[outputs])
         model.compile(optimizer=opt, loss=loss, metrics=model_metrics)
         model.fit(
             train_set,
@@ -164,11 +198,13 @@ for task in tqdm(args.task, desc="Tasks"):
                 ),
                 TqdmCallback(),
                 WandbCallback(data_type="image", predictions=5),
+                model_utils.ClearMemory(),
             ],
         )
+        initial_epoch = args.epochs[count]
         count += 1
     # Train all the model's weights
-    model.core.trainable = True
+    model.layers[-2].trainable = True
     model.compile(optimizer=opt, loss=loss, metrics=model_metrics)
     model.fit(
         train_set,
@@ -188,9 +224,17 @@ for task in tqdm(args.task, desc="Tasks"):
             ),
             TqdmCallback(),
             WandbCallback(data_type="image", predictions=5),
+            model_utils.ClearMemory(),
         ],
-        initial_epoch=args.epochs[count - 1],
+        initial_epoch=initial_epoch,
     )
     # Test the model
-    model.evaluate(test_set, callbacks=[WandbCallback()])
+    test_scores = model.evaluate(test_set)
+    wandb.log(
+        dict(
+            test_loss=test_scores[0],
+            test_accuracy=test_scores[1],
+            test_f2=test_scores[2],
+        )
+    )
     count += 1
